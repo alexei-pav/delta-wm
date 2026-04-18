90d409e5312c5aa25d3b93b684295265928380c7
Alexei R. Pavlov <loshapavlov@gmail.com>
2026-04-18 20:20:41 +0200

    fix: wallpaper set is commented by default now

--- startwm.sh
+++ startwm.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# set wallpaper
-feh --bg-scale <your_image_path>
+# set wallpaper (uncomment if needed)
+# feh --bg-scale <your_image_path>
 
 # set keyboard layout
 setxkbmap us,ru -option 'grp:alt_shift_toggle'
 
-# optional (uncomment if nesessary; dwmblocks required)
-#dwmblocks &
-
 while true; do
 	# log stderr to a file
-	dwm 2> ~/.dwm.log
+	deltawm 2> ~/.deltawm.log
 done
