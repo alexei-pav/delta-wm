c88f3e570d0b804d9e3d7f751de6aee85356ad9e
Alexei R. Pavlov <loshapavlov@gmail.com>
2026-05-01 18:44:22 +0200

    fix: renae to deltawm, change version to 0.1.1

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
-# dwm - dynamic window manager
+# deltawm - dynamic window manager
 # See LICENSE file for copyright and license details.
 
 include config.mk
 
-SRC = drw.c dwm.c util.c
+SRC = drw.c deltawm.c util.c
 OBJ = ${SRC:.c=.o}
 
 # FreeBSD users, prefix all ifdef, else and endif statements with a . for this to work (e.g. .ifdef)
 
 ifdef YAJLLIBS
-all: dwm dwm-msg
+all: deltawm deltawm-msg
 else
-all: dwm
+all: deltawm
 endif
 
 .c.o:
@@ -22,46 +22,46 @@ ${OBJ}: config.h config.mk
 config.h:
 	cp config.def.h $@
 
-dwm: ${OBJ}
+deltawm: ${OBJ}
 	${CC} -o $@ ${OBJ} ${LDFLAGS}
 
 ifdef YAJLLIBS
-dwm-msg:
-	${CC} -o $@ patch/ipc/dwm-msg.c ${LDFLAGS}
+deltawm-msg:
+	${CC} -o $@ patch/ipc/deltawm-msg.c ${LDFLAGS}
 endif
 
 clean:
-	rm -f dwm ${OBJ} dwm-${VERSION}.tar.gz
-	rm -f dwm-msg
+	rm -f deltawm ${OBJ} deltawm-${VERSION}.tar.gz
+	rm -f deltawm-msg
 
 dist: clean
-	mkdir -p dwm-${VERSION}
+	mkdir -p deltawm-${VERSION}
 	cp -R LICENSE Makefile README config.def.h config.mk\
-		dwm.1 drw.h util.h ${SRC} dwm.png transient.c dwm-${VERSION}
-	tar -cf dwm-${VERSION}.tar dwm-${VERSION}
-	gzip dwm-${VERSION}.tar
-	rm -rf dwm-${VERSION}
+		deltawm.1 drw.h util.h ${SRC} deltawm.png transient.c deltawm-${VERSION}
+	tar -cf deltawm-${VERSION}.tar deltawm-${VERSION}
+	gzip deltawm-${VERSION}.tar
+	rm -rf deltawm-${VERSION}
 
 install: all
 	mkdir -p ${DESTDIR}${PREFIX}/bin
-	cp -f dwm ${DESTDIR}${PREFIX}/bin
+	cp -f deltawm ${DESTDIR}${PREFIX}/bin
 ifdef YAJLLIBS
-	cp -f dwm-msg ${DESTDIR}${PREFIX}/bin
+	cp -f deltawm-msg ${DESTDIR}${PREFIX}/bin
 endif
-	chmod 755 ${DESTDIR}${PREFIX}/bin/dwm
+	chmod 755 ${DESTDIR}${PREFIX}/bin/deltawm
 ifdef YAJLLIBS
-	chmod 755 ${DESTDIR}${PREFIX}/bin/dwm-msg
+	chmod 755 ${DESTDIR}${PREFIX}/bin/deltawm-msg
 endif
 	mkdir -p ${DESTDIR}${MANPREFIX}/man1
-	sed "s/VERSION/${VERSION}/g" < dwm.1 > ${DESTDIR}${MANPREFIX}/man1/dwm.1
-	chmod 644 ${DESTDIR}${MANPREFIX}/man1/dwm.1
+	sed "s/VERSION/${VERSION}/g" < deltawm.1 > ${DESTDIR}${MANPREFIX}/man1/deltawm.1
+	chmod 644 ${DESTDIR}${MANPREFIX}/man1/deltawm.1
 	mkdir -p ${DESTDIR}${PREFIX}/share/xsessions
-	test -f ${DESTDIR}${PREFIX}/share/xsessions/dwm.desktop || cp -n dwm.desktop ${DESTDIR}${PREFIX}/share/xsessions
-	chmod 644 ${DESTDIR}${PREFIX}/share/xsessions/dwm.desktop
+	test -f ${DESTDIR}${PREFIX}/share/xsessions/deltawm.desktop || cp -n deltawm.desktop ${DESTDIR}${PREFIX}/share/xsessions
+	chmod 644 ${DESTDIR}${PREFIX}/share/xsessions/deltawm.desktop
 
 uninstall:
-	rm -f ${DESTDIR}${PREFIX}/bin/dwm\
-		${DESTDIR}${MANPREFIX}/man1/dwm.1\
-		${DESTDIR}${PREFIX}/share/xsessions/dwm.desktop
+	rm -f ${DESTDIR}${PREFIX}/bin/deltawm\
+		${DESTDIR}${MANPREFIX}/man1/deltawm.1\
+		${DESTDIR}${PREFIX}/share/xsessions/deltawm.desktop
 
 .PHONY: all clean dist install uninstall
--- deltawm.c
+++ deltawm.c
@@ -6,7 +6,7 @@
  * events about window (dis-)appearance. Only one X connection at a time is
  * allowed to select for this event mask.
  *
- * The event handlers of dwm are organized in an array which is accessed
+ * The event handlers of deltawm are organized in an array which is accessed
  * whenever a new event has been fetched. This allows event dispatching
  * in O(1) time.
  *
@@ -1969,7 +1969,7 @@ setup(void)
 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMCheck], XA_WINDOW, 32,
 		PropModeReplace, (unsigned char *) &wmcheckwin, 1);
 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMName], utf8string, 8,
-		PropModeReplace, (unsigned char *) "dwm", 3);
+		PropModeReplace, (unsigned char *) "deltawm", 3);
 	XChangeProperty(dpy, root, netatom[NetWMCheck], XA_WINDOW, 32,
 		PropModeReplace, (unsigned char *) &wmcheckwin, 1);
 	/* EWMH support per view */
@@ -2042,7 +2042,7 @@ spawn(const Arg *arg)
 		sigaction(SIGCHLD, &sa, NULL);
 
 		execvp(((char **)arg->v)[0], (char **)arg->v);
-		die("dwm: execvp '%s' failed:", ((char **)arg->v)[0]);
+		die("deltawm: execvp '%s' failed:", ((char **)arg->v)[0]);
 	}
 }
 
@@ -2199,7 +2199,7 @@ updatebars(void)
 		.background_pixmap = ParentRelative,
 		.event_mask = ButtonPressMask|ExposureMask
 	};
-	XClassHint ch = {"dwm", "dwm"};
+	XClassHint ch = {"deltawm", "deltawm"};
 	for (m = mons; m; m = m->next) {
 		for (bar = m->bar; bar; bar = bar->next) {
 			if (bar->external)
@@ -2406,7 +2406,7 @@ updatestatus(void)
 {
 	Monitor *m;
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
+		strcpy(stext, "deltawm-"VERSION);
 	for (m = mons; m; m = m->next)
 		drawbar(m);
 }
@@ -2509,7 +2509,7 @@ xerror(Display *dpy, XErrorEvent *ee)
 	|| (ee->request_code == X_GrabKey && ee->error_code == BadAccess)
 	|| (ee->request_code == X_CopyArea && ee->error_code == BadDrawable))
 		return 0;
-	fprintf(stderr, "dwm: fatal error: request code=%d, error code=%d\n",
+	fprintf(stderr, "deltawm: fatal error: request code=%d, error code=%d\n",
 		ee->request_code, ee->error_code);
 	return xerrorxlib(dpy, ee); /* may call exit */
 }
@@ -2525,7 +2525,7 @@ xerrordummy(Display *dpy, XErrorEvent *ee)
 int
 xerrorstart(Display *dpy, XErrorEvent *ee)
 {
-	die("dwm: another window manager is already running");
+	die("deltawm: another window manager is already running");
 	return -1;
 }
 
@@ -2550,13 +2550,13 @@ int
 main(int argc, char *argv[])
 {
 	if (argc == 2 && !strcmp("-v", argv[1]))
-		die("dwm-"VERSION);
+		die("deltawm-"VERSION);
 	else if (argc != 1)
-		die("usage: dwm [-v]");
+		die("usage: deltawm [-v]");
 	if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
 		fputs("warning: no locale support\n", stderr);
 	if (!(dpy = XOpenDisplay(NULL)))
-		die("dwm: cannot open display");
+		die("deltawm: cannot open display");
 	checkotherwm();
 	setup();
 #ifdef __OpenBSD__
