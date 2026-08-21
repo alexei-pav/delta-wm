51970487e74f419f51007f5ccf202b01393f0a05
Alexei R. Pavlov <loshapavlov@gmail.com>
2026-08-21 12:00:29 +0200

    add xsetroot to startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+export DISPLAY=:0
+
 # set wallpaper (uncomment if needed)
 # feh --bg-scale <your_image_path>
 
 # set keyboard layout
 setxkbmap us,ru -option 'grp:alt_shift_toggle'
 
+deltawm 2> /dev/null
+
+# update data (change sleep time if needed)
 while true; do
-	# log stderr to a file
-	deltawm 2> ~/.deltawm.log
+	BATT="$(/bin/cat /sys/class/power_supply/BAT0/capacity)%"
+	DATE=$(/bin/date +"%a %b %d %H:%M")
+
+	ROOT="$BATT | $DATE"
+	xsetroot -name "$ROOT"
+	sleep 2
 done
