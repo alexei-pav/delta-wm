4db81dfbd6193b93dc66773f3c40c60f9ea46dff
Alexei R. Pavlov <loshapavlov@gmail.com>
2026-08-22 12:00:07 +0200

    add battery percentage to xsetroot

--- startwm.sh
+++ startwm.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 
 export DISPLAY=:0
+BAT0_PATH=/sys/class/power_supply/BAT0
 
 # set wallpaper (uncomment if needed)
-# feh --bg-scale <your_image_path>
+# feh --bg-scale <path/to/your/wallpaper/file>
 
 # set keyboard layout
 setxkbmap us,ru -option 'grp:alt_shift_toggle'
 
-deltawm 2> /dev/null
+deltawm &2> /dev/null
+sleep 1
 
-# update data (change sleep time if needed)
 while true; do
-	BATT="$(/bin/cat /sys/class/power_supply/BAT0/capacity)%"
+	BATT="$(/bin/cat $BAT0_PATH/capacity)%"
+	B_STATUS=$(/bin/cat $BAT0_PATH/status)
+	case "$B_STATUS" in
+        "Charging")
+            icon="↑"
+            ;;
+        "Discharging")
+            icon="↓"
+            ;;
+	"Not charging")
+	    icon="$"
+	    ;;
+        "Full")
+            icon="="
+            ;;
+        *)
+            icon="?"
+            ;;
+	esac
 	DATE=$(/bin/date +"%a %b %d %H:%M")
 
-	ROOT="$BATT | $DATE"
+	ROOT="$BATT ${icon} | $DATE"
 	xsetroot -name "$ROOT"
-	sleep 2
+	sleep 2 # time between updates
 done
